bb7ed2eef7e2644b3bde360b854882a076728029
Gabriel Melo <melogoncalvesbiel@gmail.com>
2022-10-17 23:36:20 -0300

    feat: componente header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,48 @@
+import styles from "./styles.module.sass"
+
 export function Header() {
-  <header className="header">
-       
-  </header>
+  const nav = [
+    {
+      label: "Home",
+      link: "#"
+    },
+    {
+      label: "Sobre",
+      link: "#"
+    },
+    {
+      label: "Stacks",
+      link: "#"
+    },
+    {
+      label: "Projetos",
+      link: "#"
+    },
+  ]
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <div className={styles.header__content}>
+          <h1 className={styles.logo}>Gabriel Melo</h1>
+          <nav className={styles.nav}>
+            <ul className={styles.nav__list}>
+              {
+                nav.map((item) => (
+                  <li key={item.label} className={styles.nav__item}>
+                    <a className={styles.nav__link} href="#">{item.label}</a>
+                  </li>
+                ))
+              }
+            <li className={styles.nav__item}>
+              <button className={styles.header__button}>
+                Currículo
+              </button>
+            </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import styles from '../../styles/Home.module.sass'
+import { Header } from '../components/Header'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <Header />
+    </>
   )
 }
