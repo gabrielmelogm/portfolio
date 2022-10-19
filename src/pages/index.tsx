821f52e29f93d4b89875b3bdf4deeb82ba68df1a
Gabriel Melo <melogoncalvesbiel@gmail.com>
2022-10-19 09:35:08 -0300

    feat: cria elementos laterias de contato

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import styles from '../../styles/Home.module.sass'
+import { Email } from '../components/Contato/Email'
+import { SocialMedias } from '../components/Contato/SocialMedias'
 import { Header } from '../components/Header'
 import { Welcome } from '../components/Welcome'
 
@@ -6,6 +8,8 @@ export default function Home() {
   return (
     <>
       <Header />
+      <SocialMedias />
+      <Email />
       <Welcome />
     </>
   )
