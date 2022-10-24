7cd548d2df0eefbe3ea3561f5ff5c4aae536dc17
Gabriel Melo <melogoncalvesbiel@gmail.com>
2022-10-24 18:13:10 -0300

    feat: adiciona alinhamento a direita e esquerda da secao

--- src/components/Projetos/Projeto/index.tsx
+++ src/components/Projetos/Projeto/index.tsx
@@ -3,8 +3,8 @@ import { BsGithub, BsLink45Deg } from "react-icons/bs"
 import styles from "./styles.module.sass"
 
 export interface ProjetoProps {
-  key?: any
   projeto: {
+    id?: string
     thumb: StaticImageData
     category: string
     title: string
@@ -13,38 +13,76 @@ export interface ProjetoProps {
     repositoryUrl: string
     link: string
   }
+  align?: "right" | "left"
 }
 
-export function Projeto({key, projeto}: ProjetoProps) {
+export function Projeto({projeto, align, ...props}: ProjetoProps) {
   return (
-    <li key={key} className={styles.projeto__item}>
-      <div className={styles.projeto__thumb}>
-        <Image className={styles.projeto__image} src={projeto.thumb} width={870} height={490} layout="responsive" alt="Imagem do projeto" />
-      </div>
-      <div className={styles.projeto__info}>
-        <h2 className={styles.projeto__category}>{projeto.category}</h2>
-        <h2 className={styles.projeto__title}>{projeto.title}</h2>
-        <div className={styles.projeto__description}>
-          <p className={styles.projeto__text}>{projeto.description}</p>
-        </div>
-        <ul className={styles.stacks__list}>
-          {
-            projeto.stacks.map((stack) => (
-              <li key={stack} className={styles.stacks__item}>
-                {stack}
-              </li>
-            ))
-          }
-        </ul>
-        <div className={styles.projeto__link}>
-          <a className={styles.projeto__site} href={projeto.link}>
-            <BsLink45Deg />
-          </a>
-          <a className={styles.projeto__repositorio} href={projeto.repositoryUrl}>
-            <BsGithub />
-          </a>
-        </div>
-      </div>
-    </li>
+    <>
+     {
+      align === "right" ? (
+        <li className={styles.projeto__item}>
+          <div className={styles.projeto__thumb}>
+            <Image className={styles.projeto__image} src={projeto.thumb} width={870} height={490} layout="responsive" alt="Imagem do projeto" />
+          </div>
+          <div className={styles.projeto__info}>
+            <h2 className={styles.projeto__category}>{projeto.category}</h2>
+            <h2 className={styles.projeto__title}>{projeto.title}</h2>
+            <div className={styles.projeto__description}>
+              <p className={styles.projeto__text}>{projeto.description}</p>
+            </div>
+            <ul className={styles.stacks__list}>
+              {
+                projeto.stacks.map((stack) => (
+                  <li key={stack} className={styles.stacks__item}>
+                    {stack}
+                  </li>
+                ))
+              }
+            </ul>
+            <div className={styles.projeto__link}>
+              <a className={styles.projeto__site} href={projeto.link}>
+                <BsLink45Deg />
+              </a>
+              <a className={styles.projeto__repositorio} href={projeto.repositoryUrl}>
+                <BsGithub />
+              </a>
+            </div>
+          </div>
+        </li>
+      ) :
+      (
+        <li className={styles.projeto__item}>
+          <div className={`${styles.projeto__info} ${styles.info__left}`}>
+            <h2 className={styles.projeto__category}>{projeto.category}</h2>
+            <h2 className={styles.projeto__title}>{projeto.title}</h2>
+            <div className={`${styles.projeto__description} ${styles.description__left}`}>
+              <p className={styles.projeto__text}>{projeto.description}</p>
+            </div>
+            <ul className={`${styles.stacks__list} ${styles.stacks__left}`}>
+              {
+                projeto.stacks.map((stack) => (
+                  <li key={stack} className={styles.stacks__item}>
+                    {stack}
+                  </li>
+                ))
+              }
+            </ul>
+            <div className={`${styles.projeto__link} ${styles.link__left}`}>
+              <a className={styles.projeto__site} href={projeto.link}>
+                <BsLink45Deg />
+              </a>
+              <a className={styles.projeto__repositorio} href={projeto.repositoryUrl}>
+                <BsGithub />
+              </a>
+            </div>
+          </div>
+          <div className={styles.projeto__thumb}>
+            <Image className={styles.projeto__image} src={projeto.thumb} width={870} height={490} layout="responsive" alt="Imagem do projeto" />
+          </div>
+        </li>
+      )
+     }
+    </>
   )
 }
--- src/components/Projetos/index.tsx
+++ src/components/Projetos/index.tsx
@@ -1,15 +1,18 @@
 import { Container } from "../Container"
 import { Title } from "../Title"
 import { Projeto, ProjetoProps } from "./Projeto"
+import { v4 } from "uuid"
 import styles from "./styles.module.sass"
 
 import Dtmoney from "../../assets/dtmoney.png"
+import MarioJump from "../../assets/mario-jump.png"
 
 export function Projetos() {
 
   const projetos: ProjetoProps[] = [
     {
       projeto: {
+        id: v4(),
         thumb: Dtmoney,
         category: "Projeto de Desafio",
         title: "Dtmoney",
@@ -17,11 +20,23 @@ export function Projetos() {
         stacks: ["Firebase", "Create React App", "Styled Components","Docker"],
         repositoryUrl: "#",
         link: "#"
-      }
-    }
+      },
+      align: "right"
+    },
+    {
+      projeto: {
+        id: v4(),
+        thumb: MarioJump,
+        category: "Projeto Pessoal",
+        title: "Mario jump",
+        description: "A minimal, dark blue theme for VS Code, Sublime Text, Atom, iTerm, and more. Available on Visual Studio Marketplace, Package Control, Atom Package Manager, and npm.",
+        stacks: ["HTML5", "CSS3", "Vanilla Javascript"],
+        repositoryUrl: "#",
+        link: "#"
+      },
+      align: "left"
+    },
   ]
-  
-  
 
   return (
     <section className={styles.projetos}>
@@ -29,8 +44,10 @@ export function Projetos() {
         <Title>Projetos</Title>
         <ul className={styles.projetos__list}>
           {
-            projetos.map((item, index) => (
-              <Projeto key={index} projeto={item.projeto} />
+            projetos.map((item) => (
+              <>
+                <Projeto key={item.projeto.id} projeto={item.projeto} align={item.align} />
+              </>
             ))
           }
         </ul>
