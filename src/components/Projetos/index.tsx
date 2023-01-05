21b4baaea7294bf6151cba737310357f381f71cc
Gabriel Melo <melogoncalvesbiel@gmail.com>
2023-01-05 09:48:09 -0300

    feat: integracao do front com o back

--- src/components/Projetos/index.tsx
+++ src/components/Projetos/index.tsx
@@ -1,6 +1,6 @@
 import { Container } from "../Container"
 import { Title } from "../Title"
-import { Projeto, ProjetoProps } from "./Projeto"
+import { Projeto } from "./Projeto"
 import { v4 } from "uuid"
 import styles from "./styles.module.sass"
 
@@ -9,8 +9,25 @@ import MarioJump from "../../assets/mario-jump.png"
 import { useEffect, useState } from "react"
 import axios from "axios"
 
+interface ProjetoProps {
+  id?: string
+  thumb: string
+  category: string
+  title: string
+  description: string
+  stacks: {
+    name: string
+  }[]
+  repositoryUrl: string
+  link: string
+}
+
 export function Projetos() {
   const [ projetos, setProjetos ] = useState<ProjetoProps[]>([])
+  let lastElement: {
+    id: number
+    align: "left" | "right"
+  }
 
   async function getProjetos() {
     await axios.get("/api/projects")
@@ -25,19 +42,31 @@ export function Projetos() {
     getProjetos()
   }, [])
 
-  console.log(projetos)
-
   return (
     <section id="projetos" className={styles.projetos}>
       <Container>
         <Title>Projetos</Title>
         <ul className={styles.projetos__list}>
           {
-            projetos.map((projeto) => (
-              <>
-                <Projeto key={projeto.id} projeto={projeto} />
-              </>
-            ))
+            projetos.map((projeto, index) => {
+              if (index === 0) {
+                lastElement = {
+                  id: index,
+                  align: "right"
+                }
+              }
+
+              if (index >= 1) {
+                lastElement = {
+                  id: index,
+                  align: lastElement.align === "right" ? "left" : "right"
+                }
+              }
+
+              return (
+                <Projeto key={projeto.id} projeto={projeto} align={lastElement.align } />
+              )
+            })
           }
         </ul>
       </Container>
