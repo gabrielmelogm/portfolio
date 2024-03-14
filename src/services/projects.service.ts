2a07c0cce1d588bdb82ffa35c2e8479dc4a200c6
Gabriel Melo <melogoncalvesbiel@gmail.com>
2024-03-13 21:45:56 -0300

    fix: import

--- src/services/projects.service.ts
+++ src/services/projects.service.ts
@@ -1,8 +1,8 @@
-import { ProjetoProps } from "../components/Projetos/Projeto"
+import { ProjectProps } from "../components/layout/Projects/Project"
 import { gql } from "@apollo/client"
 import { client } from "../lib/graphql"
 
-export async function getProjects(): Promise<ProjetoProps[] | []> {
+export async function getProjects(): Promise<ProjectProps[] | []> {
   try {
     const GET_PROJECTS = gql`
       query {
